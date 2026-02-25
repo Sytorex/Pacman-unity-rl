3c4bf32bca067fc823e35289e73cdb19d45533de
Paul CAUCHE <paul.cauche@student.junia.com>
2026-02-25 14:23:41 +0100

    new cord for scatter

--- Scripts/GhostHome.cs
+++ Scripts/GhostHome.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class GhostHome : GhostBehavior
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // On n'ajoute pas "float speed" ici car elle est déjà dans GhostBehavior
 
-    // Update is called once per frame
+    // Position de sortie (Centre de la porte)
+    private Vector3 exitPoint = new Vector3(13.5f, -11.5f, 0);
+
     void Update()
     {
-        
+        // On utilise "speed" hérité de GhostBehavior (si elle est public/protected)
+        // Sinon, remplace par une valeur fixe comme 4f pour tester
+        float moveSpeed = 4f;
+
+        // Déplacement vers la sortie
+        transform.position = Vector3.MoveTowards(transform.position, exitPoint, moveSpeed * Time.deltaTime);
+
+        // Si on est arrivé à la porte
+        if (Vector3.Distance(transform.position, exitPoint) < 0.1f)
+        {
+            transform.position = exitPoint;
+            this.Disable(); // Désactive GhostHome
+
+            // Active le comportement suivant (Chase)
+            GetComponent<GhostChase>().Enable();
+        }
     }
 }
--- Scripts/GhostScatter.cs
+++ Scripts/GhostScatter.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class GhostScatter : GhostBehavior
 {
-    private Vector3[] topLeftCorner = { new Vector3(1, 1, 0), new Vector3(1, 8, 0), new Vector3(12,1,0), new Vector3(12,8,0)};
-    private Vector3[] topRightCorner = { new Vector3(31, 1, 0) , new Vector3(31,8,0), new Vector3(19,1,0), new Vector3(19, 8,0)};
-    private Vector3[] bottomLeftCorner = { new Vector3(1, 27, 0), new Vector3(1, 18, 0), new Vector3(12, 27, 0), new Vector3(12, 18, 0) };
-    private Vector3[] bottomRightCorner = { new Vector3(31,27, 0), new Vector3(31, 18, 0), new Vector3(19, 27, 0), new Vector3(19, 18, 0) };
+    private Vector3[] topLeftCorner = { new Vector3(1.5, -0.5, 0), new Vector3(1.5, -5, 0), new Vector3(6.5,-5,0), new Vector3(6.5,-0.5,0)};
+    private Vector3[] topRightCorner = { new Vector3(26.5, -0.5, 0) , new Vector3(26.5, -5, 0), new Vector3(21.5,-5,0), new Vector3(21.5,-0.5,0)};
+    private Vector3[] bottomLeftCorner = { new Vector3(1.5, -25.5, 0), new Vector3(1.5, 18, 0), new Vector3(12.5, -25.5, 0), new Vector3(12.5, -28.5, 0) };
+    private Vector3[] bottomRightCorner = { new Vector3(26.5, -28.5, 0), new Vector3(15.5, -28.5, 0), new Vector3(15.5, -25.5, 0), new Vector3(26.5, -25.5, 0) };
 
     private Vector3[] scatterTargetList;
     private Vector3 scatterTarget;
